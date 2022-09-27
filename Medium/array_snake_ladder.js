846845b332e35ba66aa18d74a7c8a6345ea4b772
sangeeth <sangeeth.s@neoito.com>
2022-09-27 12:01:09 +0530

    removed unwanted console

--- Medium/array_snake_ladder.js
+++ Medium/array_snake_ladder.js
@@ -3,14 +3,14 @@
 function snakeLadderArr() {
     let arr = [];
     for (let i = 1; i <= 64; i++) {
-        console.log(Math.floor((i - 1) / 8), i, (i - 1) % 8)
-        const base = Math.floor((i - 1) / 8);
+        const base = Math.floor((i - 1) / 10);
         if (base % 2 === 0) {
             arr.push(i);
         } else {
-            arr.push(((base + 1) * 8) - ((i - 1) % 8));
+            arr.push(((base + 1) * 10) - ((i - 1) % 10));
         }
     }
+    return arr;
 }
 
 console.log(snakeLadderArr());
